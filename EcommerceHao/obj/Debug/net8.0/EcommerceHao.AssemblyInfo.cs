a11f43f32538b4c8f54c70b96c87d1afc7817cde
HaoLam2608 <haolam2684@gmail.com>
2024-07-05 01:55:59 +0700

    MVC 04/07

--- EcommerceHao/obj/Debug/net8.0/EcommerceHao.AssemblyInfo.cs
+++ EcommerceHao/obj/Debug/net8.0/EcommerceHao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceHao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e9ea618c57f20eef3908052a5e85e6bfd83f132")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceHao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceHao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
